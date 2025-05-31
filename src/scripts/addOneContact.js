d603ff39701c098fc16a1057a722e7f6e9d1a43e
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-05-31 21:25:52 +0300

    add generateContacts and addOneContact

--- src/scripts/addOneContact.js
+++ src/scripts/addOneContact.js
@@ -1,3 +1,16 @@
-export const addOneContact = async () => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+export const addOneContact = async () => {
+  try {
+    const allContacts = await readContacts();
+    const newContact = createFakeContact();
+    allContacts.push(newContact);
+    await writeContacts(allContacts);
+  } catch (error) {
+    console.log('помилка', error.message);
+  }
+};
 
 addOneContact();
--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,19 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from '../utils/createFakeContact.js';
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const generateContacts = async (number) => {
+  try {
+    const allContacts = await readContacts();
+
+    for (let i = 0; i < number; i++) {
+      const contact = await createFakeContact();
+      return allContacts.push(contact);
+    }
+    await writeContacts(allContacts);
+  } catch (error) {
+    console.log('помилка', error.message);
+  }
+};
 
 generateContacts(5);
