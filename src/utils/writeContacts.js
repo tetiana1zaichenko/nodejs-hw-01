2f994a6c002403e568f959d486e889068ef9faf5
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-05-31 20:48:12 +0300

    add writeContacts

--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -1,3 +1,13 @@
+import fs from 'fs/promises';
 import { PATH_DB } from '../constants/contacts.js';
 
-export const writeContacts = async (updatedContacts) => {};
+export const writeContacts = async (updatedContacts) => {
+  try {
+    const data = JSON.stringify(updatedContacts, null, 2);
+    await fs.writeFile(PATH_DB, data, 'utf8');
+    console.log('Дані записані');
+  } catch (error) {
+    console.log('Помилка', error.message);
+    throw error;
+  }
+};
