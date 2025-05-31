ecd30e9997ebc81767dbbbc93e7dda62bfb36f27
Tetiana_Zaichenko <tanja.zaychenko@gmail.com>
2025-05-31 21:33:50 +0300

    add getAllContacts and countContacts

--- src/scripts/countContacts.js
+++ src/scripts/countContacts.js
@@ -1,3 +1,12 @@
-export const countContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const countContacts = async () => {
+  try {
+    const count = (await readContacts()).length;
+    return count;
+  } catch (error) {
+    console.log('помилка', error.message);
+  }
+};
 
 console.log(await countContacts());
--- src/scripts/getAllContacts.js
+++ src/scripts/getAllContacts.js
@@ -1,3 +1,12 @@
-export const getAllContacts = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+
+export const getAllContacts = async () => {
+  try {
+    const contacts = await readContacts();
+    return contacts;
+  } catch (error) {
+    console.log('Помилка', error.message);
+  }
+};
 
 console.log(await getAllContacts());
